2a03dbcd76eb322bc46bf57b64c763a526a51c42
Evgeniy Makarchyk <zenya1707.44@gmail.com>
2023-12-23 11:31:56 +0100

    добавили функцию сортировки

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,16 @@ def load_file(file_path):
         data = json.load(file)
     return data
 
+
+def sort_list(data, amount=5):
+    """
+        Создаёт новый EXECUTED список
+    """
+    new_list = []
+    for element in data:
+        if not element or element['state'] != 'EXECUTED':
+            continue
+        new_list.append(element)
+        new_list = sorted(new_list, key=lambda operation: operation['date'], reverse=True)
+    return new_list[:amount]
+
