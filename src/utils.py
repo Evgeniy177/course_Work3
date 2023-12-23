cd52a6ac9521783066da9ce0a8cebe3a51b8ade0
Evgeniy Makarchyk <zenya1707.44@gmail.com>
2023-12-23 11:36:19 +0100

    добавили функцию форматирования счета

--- src/utils.py
+++ src/utils.py
@@ -31,3 +31,19 @@ def format_date(data):
     """
     date_o = datetime.strptime(data["date"], "%Y-%m-%dT%H:%M:%S.%f")
     return date_o.strftime("%d.%m.%Y")
+
+
+def get_requisites(data):
+    """
+    Форматирование номера счета
+    """
+    list_of_data = data.split(' ')
+    if len(list_of_data[-1]) == 16:
+        card_number = list_of_data[-1][0:6] + '*' * 6 + list_of_data[-1][-4:]
+        formatted_data = card_number[:4] + ' ' + card_number[4:8] + ' ' + card_number[8:12] + ' ' + card_number[12:]
+        list_of_data.pop(-1)
+        return ' '.join(list_of_data), formatted_data
+    else:
+        formatted_data = '**' + list_of_data[-1][-4:]
+        list_of_data.pop(-1)
+        return ' '.join(list_of_data), formatted_data
